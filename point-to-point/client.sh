496b54a594635173503475b3c7adaa5b16e0bba9
gomesuit <gomesuit@gmail.com>
2017-07-04 03:41:59 +0900

    save

--- point-to-point/client.sh
+++ point-to-point/client.sh
@@ -6,3 +6,9 @@ sudo yum install -y vim
 yum install -y epel-release
 yum install -y openvpn
 
+ln -sf /vagrant/settings/client.conf /etc/openvpn/client.conf
+
+# openvpn --client --config /etc/openvpn/client.conf
+
+yum install -y traceroute
+# traceroute -p 1194 192.168.33.10
--- point-to-point/server.sh
+++ point-to-point/server.sh
@@ -6,6 +6,10 @@ sudo yum install -y vim
 yum install -y epel-release
 yum install -y openvpn
 
-cp /vagrant/settings/server.conf /etc/openvpn/
+ln -s /vagrant/settings/server.conf /etc/openvpn/server.conf
 
 openvpn --genkey --secret /vagrant/static.key
+
+# openvpn --config /etc/openvpn/server.conf
+
+yum install -y traceroute
