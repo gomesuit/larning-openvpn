9ce82d6d1742e8603ffdca01d0f0a77c2ed6e780
gomesuit <gomesuit@gmail.com>
2017-07-06 03:46:52 +0900

    save

---
+++ PKI/client.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+sudo yum install -y vim
+
+yum install -y epel-release
+yum install -y openvpn
+
+# ln -sf /vagrant/settings/client.conf /etc/openvpn/client.conf
+
+# openvpn --client --config /etc/openvpn/client.conf
+
+# yum install -y traceroute
+# traceroute -U -p 1194 192.168.33.10
---
+++ PKI/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+sudo yum install -y vim
+
+yum install -y epel-release
+yum install -y openvpn easy-rsa
+
+# ln -s /vagrant/settings/server.conf /etc/openvpn/server.conf
+
+# openvpn --genkey --secret /vagrant/static.key
+
+# openvpn --config /etc/openvpn/server.conf
+
+# yum install -y traceroute
