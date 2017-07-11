cd507381f83a91bf710b06d5da477a683cdbb990
gomesuit <gomesuit@gmail.com>
2017-07-12 01:27:34 +0900

    save

--- routing/server.sh
+++ routing/server.sh
@@ -15,4 +15,14 @@ yum install -y openvpn easy-rsa
 # ./build-dh
 
 yum install -y traceroute
+
+# パケット転送をオンにする
+echo 1 > /proc/sys/net/ipv4/ip_forward
+#echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+
+# VPNクライアントのトラフィックをインターネットにNATで接続させる
+# enp0s8の部分は環境により異なる
+# iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o enp0s8 -j MASQUERADE
+
+# openvpn 起動
 # openvpn /vagrant/settings/server.conf
